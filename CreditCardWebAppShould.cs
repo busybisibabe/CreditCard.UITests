fddb45ace376a326db84b485dd3b8b391eb0cba3
Bissy <busybisibabe@gmail.com>
2023-03-20 15:07:25 +0000

    Add project files.

---
+++ CreditCardWebAppShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCard.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+            }
+
+        }
+
+
+
+
+    }
+}
